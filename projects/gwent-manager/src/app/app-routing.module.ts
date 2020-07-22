fe69af87f56855d01b96e7ce5851a963d2b3faab
RaulCronos <cronosworld@gmail.com>
2020-07-22 20:55:18 +0200

    adds routes

--- projects/gwent-manager/src/app/app-routing.module.ts
+++ projects/gwent-manager/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewCardFormComponent } from './new-card-form/new-card-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'new-card', component: NewCardFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
